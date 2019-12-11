a88bcce75b4db2d95c67206e2ccc94c766aaaa3b
rosen1000 <rosen.ruseff@gmail.com>
2019-12-11 13:26:05 -0800

    Made expandable voice channels prototype ready to use

--- handlers/ready.js
+++ handlers/ready.js
@@ -24,5 +24,26 @@ module.exports = (bot) => {
             }
             bot.user.setActivity(text, { type: status.type });
         }, ms("30m"));
+
+        let channelJoin = bot.channels.find(ch => ch.id == 654318855346323467);
+        if (channelJoin) {
+            console.log("Channel found!");
+            bot.on("voiceStateUpdate", async (oldMember, newMember) => {
+                // if (oldMember.guild.id != 417384366893826049) return;
+                let expandableChannels = newMember.guild.channels.filter(ch => ch.type == "voice" && ch.name.includes("New Channel"));
+                if (newMember.voiceChannel == channelJoin) {
+                    let channel = await channelJoin.clone("New Channel " + (parseInt(expandableChannels.size) + 1));
+                    channel.setParent(newMember.voiceChannel.parent);
+                    channel.setPosition(newMember.voiceChannel.position + 1);
+                    newMember.setVoiceChannel(channel);
+                }
+                if (expandableChannels.find(c => c.id == oldMember.voiceChannelID)) {
+                    let target = await oldMember.guild.channels.find(ch => ch.id == oldMember.voiceChannelID);
+                    if (target.members.size == 0) {
+                        await target.delete();
+                    }
+                }
+            });
+        }
     });
 }
