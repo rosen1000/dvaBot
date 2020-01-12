fd9fb7871ab5755503c8b3bb362466f12f9c3946
rosen1000 <rosen.ruseff@gmail.com>
2020-01-12 05:28:57 +0200

    some changes to the mongoose and schemas

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.DB);
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
 module.exports = mongoose;
--- models/server.js
+++ models/server.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
 const serverSchema = mongoose.Schema({
-    id: Number,
-    prefix: String,
-    coinType: String,
-    coinName: String
+    id: String,
+    prefix: {
+        default: "?",
+        type: String
+    },
+    coinType: {
+        default: "dorito",
+        type: String
+    },
+    coinName: {
+        default: "Doritos",
+        type: String
+    },
+    coinCustom: {
+        default: false,
+        type: Boolean
+    }
+},
+{
+    strict: false
 });
 
 module.exports = mongoose.model("server", serverSchema);
