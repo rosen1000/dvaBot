6d1d709e9f72188845a2280821a7155a15f68711
rosen1000 <rosen.ruseff@gmail.com>
2020-03-09 02:30:23 +0200

    translated the feedback command

---
+++ commands/info/feedback.js
@@ -0,0 +1,20 @@
+const { RichEmbed } = require("discord.js")
+
+module.exports = {
+    name: "feedback",
+    type: "info",
+    desc: "Send feedback to the bot owner",
+    use: "<text>",
+    run: async (bot, message, args) => {
+        if (!args[0]) return message.channel.send("Please tell me something");
+        let feedback = args.join(" ");
+        let embed = new RichEmbed()
+            .setColor(require("../../botconfig.json").color)
+            .setAuthor(message.author.tag)
+            .setThumbnail(message.author.displayAvatarURL)
+            .addField("Feedback:", feedback)
+            .setFooter("In server: " + message.guild.name);
+        bot.users.get("353464955217117185").send(embed);
+        message.channel.send("👍");
+    }
+}
