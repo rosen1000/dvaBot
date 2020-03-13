c6d2f76713fb14043e97d9054bbfd38a22074ee5
rosen1000 <rosen.ruseff@gmail.com>
2020-03-13 16:38:55 +0200

    more unarchive work

---
+++ commands/info/invite.js
@@ -0,0 +1,15 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "invite",
+    type: "info",
+    desc: "Sends invite link for the bot",
+    use: "",
+    run: async (bot, message, args) => {
+        let embed = RichEmbed()
+            .setColor(require("../../botconfig.json"))
+            .setTitle("Invite:")
+            .setDescription("[Invite me](https://discordapp.com/api/oauth2)")
+        message.channel.send(embed);
+    }
+}
