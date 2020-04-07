d782fc59236f43a0dedb38658e3a36c5a8d6400c
rosen1000 <rosen.ruseff@gmail.com>
2020-04-07 11:36:24 +0300

    invite.js fix

--- commands/invite.js
+++ commands/invite.js
@@ -2,11 +2,10 @@ const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-    let invEmbed = new Discord.RichEmbed()
+    let invEmbed = new Discord.MessageEmbed()
         .setColor(botconfig.color)
         .setTitle('Invite:')
-        .setDescription("[invite me](https://discordapp.com/api/oauth2/authorize?client_id=455489457789861898&permissions=8&scope=bot) ❤"
-            + "\n[link to my server](https://discord.gg/PPPyzUb)");
+        .setDescription("[invite me](https://discordapp.com/api/oauth2/authorize?client_id=455489457789861898&permissions=8&scope=bot) ❤");
 
     message.channel.send(invEmbed)
 }
