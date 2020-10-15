403c1074a391bac8ed9217f682e20213f519f6d3
rosen1000 <rosen.ruseff@gmail.com>
2020-10-15 12:31:10 +0300

    improved avatar search and size

--- commands/image/avatar.js
+++ commands/image/avatar.js
@@ -10,19 +10,23 @@ module.exports.run = async (bot, message, args) => {
     let aMember =
         message.guild.members.cache.get(args[0]) ||
         message.guild.members.cache.find(
-            (m) =>
-                m.user.username == args.join(" ") || m == message.mentions.members.first()
-        );
+            (m) => m.user.username == args.join(" ") || m == message.mentions.members.first()
+        ) ||
+        bot.users.cache.find((u) => u.id == args[0]);
     let embed = new Discord.MessageEmbed().setColor(botconfig.color);
 
     if (!aMember)
         embed
             .setAuthor(`${message.author.username}'s avatar`)
-            .setImage(message.author.avatarURL());
-    else
+            .setImage(message.author.avatarURL() + "?size=1024")
+            .setURL(message.author.avatarURL() + "?size=1024");
+    else {
+        if (aMember.user) aMember = aMember.user;
         embed
-            .setAuthor(`${aMember.user.username}'s avatar`)
-            .setImage(aMember.user.avatarURL());
+            .setAuthor(`${aMember.username}'s avatar`)
+            .setImage(aMember.avatarURL() + "?size=1024")
+            .setURL(aMember.avatarURL() + "?size=1024");
+    }
 
     message.channel.send(embed);
 };
