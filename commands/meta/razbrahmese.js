6be45b670f1f88f21be5a09a1e968790a92f38a1
rosen1000 <rosen.ruseff@gmail.com>
2020-12-01 09:58:30 +0200

    fix

--- commands/meta/razbrahmese.js
+++ commands/meta/razbrahmese.js
@@ -1,17 +1,11 @@
 const Discord = require("discord.js");
 
-module.exports = {
+module.exports.run = async (bot, message, args) => {
+    message.channel.send("https://media.discordapp.net/attachments/782664145056956458/783237833347170324/IMG_20201201_094705.jpg?width=373&height=640");
+}
+module.exports.help = {
     name: "razbrahmese",
-    category: "meta",
-    description: "dont mind this, its an inside joke",
-    use: "razbrahmese",
-    enabled: true,
-    /**
-     * @param {Discord.Client} bot
-     * @param {Discord.Message} message
-     * @param {string[]} args
-     */
-    run: async (bot, message, args) => {
-        message.channel.send("https://media.discordapp.net/attachments/782664145056956458/783237833347170324/IMG_20201201_094705.jpg?width=373&height=640")
-    }
+    type: "meta",
+    desc: "dont mind this, its an inside joke",
+    use: "razbrahmese"
 }
